55f263e042697f1d36bef80bf99915a0c450bdee
denverquane <chewysnarf@gmail.com>
2021-02-01 19:54:20 -0700

    Better lock settings

--- internal/redis/lock.go
+++ internal/redis/lock.go
@@ -2,10 +2,12 @@ package redis
 
 import (
 	"github.com/go-redsync/redsync/v4"
+	"time"
 )
 
+// locks have 5 second duration, 3 seconds total of retries, retries every 500 ms
 func LockSnowflake(locker *redsync.Redsync, snowflake string) (*redsync.Mutex, error) {
-	mutex := locker.NewMutex(snowflake)
+	mutex := locker.NewMutex(snowflake, redsync.WithExpiry(time.Second*5), redsync.WithRetryDelay(time.Millisecond*500), redsync.WithTries(6))
 	err := mutex.Lock()
 	if err != nil {
 		return nil, err
